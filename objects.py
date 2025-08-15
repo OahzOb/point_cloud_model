ce0268e4ac129bd895b882d50978635086470649
OahzOb <zhaoby1234@gmail.com>
2025-08-15 08:25:33 +0800

    Maybe we should start from 3d, since 2d is needed by 3d, 3d can help us kown what do we need to set in 2d class.

--- objects.py
+++ objects.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 import numpy as np
+
+
 class Shape2d(ABC):
+
     @property
     @abstractmethod
     def area(self):
@@ -16,12 +19,10 @@ class Square(Shape2d):
             edges = edges[0] * 4
         self.edges = edges
         self._area = self.edges[0] * self.edges[2]
-
     
     @property
     def area(self):
         return self._area
-
 
     @classmethod
     def from_edges(cls, edge):
@@ -34,7 +35,6 @@ class Rectangle(Shape2d):
         self.edges = edges
         self._area = self.edge[1] * self.edge[2]
 
-
     @property
     def area(self):
         return self._area
@@ -46,7 +46,6 @@ class Triangle(Shape2d):
         self.radians = radians
         self.angles = angles
         self._area = self.edges[0] * np.sin(self.radians[0]) * self.edges[1] / 2
-
 
     @property
     def area(self):
@@ -62,3 +61,16 @@ class Circle(Shape2d):
     @property
     def area(self):
         return self._area
+
+
+class Shape3d(ABC):
+
+    @property
+    @abstractmethod
+    def volume(self):
+        pass
+
+    @property
+    @abstractmethod
+    def area(self):
+        pass
